4f7d421fcfce92d23becf4ee5eecdafc2827cb62
Abhishek Asgola <abhishekasgola09@gmail.com>
2024-08-03 02:58:20 +0530

    Delete Backend/~/vscode-root directory

--- Backend/~/vscode-root/User/History/2ed75e70/w8kR.ts
+++
@@ -1,2 +0,0 @@
-export const domain = 'your-auth0-domain.com';
-export const audience = 'https://your-api-identifier.com';
--- Backend/~/vscode-root/User/History/60b8002b/qcer.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-const authenticate = async (req: Request, res: Response, next: NextFunction) => {
-  await auth(req, res, next, config).catch(next);
-};
-
-module.exports = authenticate;
